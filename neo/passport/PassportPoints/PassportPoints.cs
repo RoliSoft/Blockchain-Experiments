147b7cd7d6ea5836a97cc9b6e2cb1120897363c1
RoliSoft <rolisoft@gmail.com>
2018-03-01 22:27:08 +0200

    Add port of PassportPoints.sol to the NEO platform in C#.

---
+++ neo/passport/PassportPoints/PassportPoints.cs
@@ -0,0 +1,53 @@
+﻿using System.Numerics;
+
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework;
+
+namespace PassportPoints
+{
+    public class PassportPoints : SmartContract
+    {
+        private const string PointsPrefix = "points_";
+
+        private static readonly byte[] Owner = "AbTu9zV73T25nQeHg4BdhWrBq6m3ASQerc".ToScriptHash();
+
+        public static object Main(string operation, params object[] args)
+        {
+            switch (operation)
+            {
+                case nameof(IssuePoints):
+                    return IssuePoints((string)args[0], (BigInteger)args[1]);
+
+                case nameof(GetPoints):
+                    return GetPoints((string)args[0]);
+
+                default:
+                    return false;
+            }
+        }
+
+        public static bool IssuePoints(string name, BigInteger quantity)
+        {
+            if (!Runtime.CheckWitness(Owner))
+            {
+                Runtime.Log("Only owner can issue points.");
+                return false;
+            }
+
+            if (quantity < 0)
+            {
+                Runtime.Log("Cannot issue negative points.");
+                return false;
+            }
+            
+            Storage.Put(Storage.CurrentContext, PointsPrefix + name, quantity + GetPoints(name));
+
+            return true;
+        }
+
+        public static BigInteger GetPoints(string name)
+        {
+            return Storage.Get(Storage.CurrentContext, PointsPrefix + name).AsBigInteger();
+        }
+    }
+}
---
+++ neo/passport/SmartContractUnitTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+using Neo.Cryptography;
+using Neo.VM;
+
+namespace SmartContractUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new ExecutionEngine(null, Crypto.Default);
+            engine.LoadScript(File.ReadAllBytes(@"..\..\..\PassportPoints\bin\PassportPoints.avm"));
+
+            CallFunction(engine, "IssuePoints", "Roland", 25);
+            CallFunction(engine, "GetPoints", "Roland");
+            
+            Console.WriteLine($"Execution result: {GetBigIntegerReturn(engine)}");
+            Console.ReadLine();
+        }
+
+        static void CallFunction(ExecutionEngine engine, params object[] args)
+        {
+            using (var sb = new ScriptBuilder())
+            {
+                foreach (var arg in args.Reverse())
+                {
+                    sb.EmitPush(arg);
+                }
+
+                engine.LoadScript(sb.ToArray());
+            }
+
+            engine.Execute();
+        }
+
+        static BigInteger GetBigIntegerReturn(ExecutionEngine engine)
+        {
+            return engine.EvaluationStack.Pop().GetBigInteger();
+        }
+    }
+}
