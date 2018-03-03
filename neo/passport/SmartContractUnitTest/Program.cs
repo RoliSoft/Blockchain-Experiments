6e1d3fdd201991f706c281866f88f9314eafb5a8
RoliSoft <rolisoft@gmail.com>
2018-03-03 16:47:45 +0200

    Add method to unit test project which invokes smart contract functions with variadic arguments.

--- neo/passport/SmartContractUnitTest/Program.cs
+++ neo/passport/SmartContractUnitTest/Program.cs
@@ -4,21 +4,24 @@ using System.Linq;
 using System.Numerics;
 
 using Neo.Cryptography;
+using Neo.SmartContract;
 using Neo.VM;
 
 namespace SmartContractUnitTest
 {
+    using System.Collections.Generic;
+
     class Program
     {
         static void Main(string[] args)
         {
             var engine = new ExecutionEngine(null, Crypto.Default);
             engine.LoadScript(File.ReadAllBytes(@"..\..\..\PassportPoints\bin\PassportPoints.avm"));
-
-            CallFunction(engine, "IssuePoints", "Roland", 25);
-            CallFunction(engine, "GetPoints", "Roland");
             
-            Console.WriteLine($"Execution result: {GetBigIntegerReturn(engine)}");
+            CallVarFunction(engine, "IssuePoints", "Roland", 25);
+            CallVarFunction(engine, "GetPoints", "Roland");
+
+            Console.WriteLine($"Execution result: {GetBytesReturn(engine)}");
             Console.ReadLine();
         }
 
@@ -37,9 +40,56 @@ namespace SmartContractUnitTest
             engine.Execute();
         }
 
+        static void CallVarFunction(ExecutionEngine engine, string operation, params object[] args)
+        {
+            using (var sb = new ScriptBuilder())
+            {
+                var paramArray = new List<ContractParameter>();
+
+                foreach (var arg in args)
+                {
+                    ContractParameterType type;
+
+                    if (arg is string)
+                    {
+                        type = ContractParameterType.String;
+                    }
+                    else if (arg is int || arg is BigInteger)
+                    {
+                        type = ContractParameterType.Integer;
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Unsupported type.");
+                    }
+
+                    paramArray.Add(new ContractParameter(type) { Value = arg });
+                }
+
+                var arrayOfArgs = new ContractParameter(ContractParameterType.Array) { Value = paramArray.ToArray() };
+
+                sb.EmitPush(arrayOfArgs);
+                sb.EmitPush(operation);
+
+                engine.LoadScript(sb.ToArray());
+            }
+
+            engine.Execute();
+        }
+
+        static string GetStringReturn(ExecutionEngine engine)
+        {
+            return engine.EvaluationStack.Pop().GetString();
+        }
+
         static BigInteger GetBigIntegerReturn(ExecutionEngine engine)
         {
             return engine.EvaluationStack.Pop().GetBigInteger();
+        }
+
+        static string GetBytesReturn(ExecutionEngine engine)
+        {
+            return BitConverter.ToString(engine.EvaluationStack.Pop().GetByteArray());
         }
     }
 }
